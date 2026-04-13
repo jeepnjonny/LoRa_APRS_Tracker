39cdf5c85e3d7ee605df46d97aea2c0dc92c5dc2
jeepnjonny <40327904+jeepnjonny@users.noreply.github.com>
2026-04-13 14:46:48 -0600

    Update LoRa_APRS_Tracker.cpp

--- src/LoRa_APRS_Tracker.cpp
+++ src/LoRa_APRS_Tracker.cpp
@@ -1,7 +1,7 @@
 /* Copyright (C) 2025 Ricardo Guzman - CA2RXU
  *
  * This file is part of LoRa APRS Tracker.
- *
+ * MY CHange
  * LoRa APRS Tracker is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
